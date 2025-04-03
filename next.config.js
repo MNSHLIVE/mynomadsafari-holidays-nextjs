3f9066c26d97b2e7f58c7b5914bc69acdd385a47
MNSHLIVE <mnshlive@gmail.com>
2025-04-04 00:12:22 +0530

    Simplify deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    domains: ['*'],
     remotePatterns: [
       {
         protocol: 'https',
@@ -10,22 +8,12 @@ const nextConfig = {
       },
     ],
   },
-  // Optimize development performance
-  webpack: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300
-    }
-    return config
-  },
   typescript: {
     ignoreBuildErrors: true
   },
   eslint: {
     ignoreDuringBuilds: true
-  },
-  // Configure page extensions
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js']
+  }
 }
 
 module.exports = nextConfig 
