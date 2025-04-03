57fca36065740084c73da882a8581fa3d0bbe556
MNSHLIVE <mnshlive@gmail.com>
2025-04-03 20:06:01 +0530

    Update configuration for server output mode

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: 'server',
+  distDir: '.next',
   images: {
     domains: ['localhost'],
     remotePatterns: [
